d90138029ea9bb601080eb0f934728a5b0ec5141
FernandoGames <fernandonunesag@outlook.com>
2025-01-16 21:18:16 -0300

    finished front of auth

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -31,8 +31,14 @@ const RooyLayout = () => {
       }
     return (
         <Stack>
-           <Stack.Screen name="index" options={{ headerShown:
+            <Stack.Screen name="index" options={{ headerShown:
+            false }} />                
+            <Stack.Screen name="(auth)" options={{ headerShown:
             false }} /> 
+            <Stack.Screen name="(tabs)" options={{ headerShown:
+            false }} /> 
+            {/* <Stack.Screen name="/search/[query]" options={{ headerShown:
+            false }} /> */}
         </Stack>
     )
 }
